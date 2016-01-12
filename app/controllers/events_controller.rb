7944cb4e56df3f5dbff15b7cf04921e7b1f46696
Euan Walls <apps@inocr8.co>
2016-01-12 23:08:21 +0000

    controller update and views for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,16 @@
 class EventsController < ApplicationController
+
+  before_action :load_event, except:[:index, :create, :new]
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
   def index
+    @events = Event.all
+  end
+
+  def create
+    current_user.events.create(event_params)
+    redirect_to events_path
   end
 
   def show
@@ -8,6 +19,30 @@ class EventsController < ApplicationController
   def edit
   end
 
+  def update
+    @event.update(event_params)
+    redirect_to events_path
+  end
+
   def new
+    @event = Event.new
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to(events_path)
+  end
+
+  # def self.next
+  #   upcoming.first
+  # end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :short, :description, :event_image, :speaker_id, :crowd_id, :venue_id, :tag_id)
+    end
+
+  def load_event
+    @event = Event.find(params[:id])
   end
 end
