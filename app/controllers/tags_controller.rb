a2c03c274629ee75cc40887c787732eb96fa7b7a
Euan Walls <apps@inocr8.co>
2016-01-13 02:28:39 +0000

    update to controller for Tag and new views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,56 @@
 class TagsController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+    before_action :load_tag, except:[:index, :create, :new]
+    before_action :authenticate_user!
+    load_and_authorize_resource
 
-  def new
-  end
+    def index
+      @tags = Tag.all
+    end
 
-  def edit
+    def new
+      @tag = Tag.new
+    end
+
+    def create
+      @tag = Tag.new(tag_params)
+
+      respond_to do |format|
+        if @tag.save
+          format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        else
+          format.html { render :new }
+        end
+      end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @tag.update(tag_params)
+          format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+
+    def destroy
+      @tag.destroy
+      redirect_to(tags_path)
+    end
+
+    private
+      def tag_params
+        params.require(:tag).permit(:name, :description)
+      end
+
+    def load_tag
+      @tag = Tag.find(params[:id])
+    end
   end
-end
